edba132a96d1169a5cc0b2ed1cd4e51f0c1fbdeb
lumine <463305152@qq.com>
2024-10-09 15:55:16 +0800

    ae2 mixin

---
+++ src/main/java/io/github/cpearl0/ctnhcore/mixin/appeng/CraftingCpuLogicMixin.java
@@ -0,0 +1,44 @@
+package io.github.cpearl0.ctnhcore.mixin.appeng;
+
+import appeng.api.networking.energy.IEnergyService;
+import appeng.crafting.execution.CraftingCpuLogic;
+import appeng.crafting.execution.ElapsedTimeTracker;
+import appeng.crafting.execution.ExecutingCraftingJob;
+import appeng.me.cluster.implementations.CraftingCPUCluster;
+import appeng.me.service.CraftingService;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.lang.reflect.Field;
+
+@Mixin(CraftingCpuLogic.class)
+public class CraftingCpuLogicMixin {
+
+    @Shadow private ExecutingCraftingJob job;
+    @Shadow @Final private CraftingCPUCluster cluster;
+    long tickcount  = 0L;
+    @Inject(method = "tickCraftingLogic", at = @At(value = "INVOKE", target = "Lappeng/me/cluster/implementations/CraftingCPUCluster;getCoProcessors()I"), cancellable = true, remap = false)
+    private void tick(IEnergyService eg, CraftingService cc, CallbackInfo ci){
+        tickcount = this.cluster.getLevel().getGameTime();
+
+        long stored = 0;
+
+        try {
+            Class<?> clazz = job.getClass();
+            Field field = clazz.getDeclaredField("timeTracker");
+            field.setAccessible(true);
+            ElapsedTimeTracker timeTracker = (ElapsedTimeTracker) field.get(job);
+            stored = timeTracker.getRemainingItemCount();
+        }catch (NoSuchFieldException | IllegalAccessException ignored) {
+        }
+
+        if(tickcount % 20 != 0 && stored > 1000000)
+        {
+            ci.cancel();
+        }
+    }
+}
