205e16d04ffaf0099c09f885ec15a19e5688ac9c
CPearl0 <1269596929@qq.com>
2024-10-12 10:49:12 +0800

    replace missing blocks

--- src/main/java/io/github/cpearl0/ctnhcore/mixin/appeng/CraftingCpuLogicMixin.java
+++ src/main/java/io/github/cpearl0/ctnhcore/mixin/appeng/CraftingCpuLogicMixin.java
@@ -9,21 +9,25 @@ import appeng.me.service.CraftingService;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.lang.reflect.Field;
 
-@Mixin(CraftingCpuLogic.class)
+@Mixin(value = CraftingCpuLogic.class, remap = false)
 public class CraftingCpuLogicMixin {
+    @Shadow
+    private ExecutingCraftingJob job;
+    @Shadow @Final
+    CraftingCPUCluster cluster;
+    @Unique
+    long CTNHCore$tickCount = 0L;
 
-    @Shadow private ExecutingCraftingJob job;
-    @Shadow @Final private CraftingCPUCluster cluster;
-    long tickcount  = 0L;
-    @Inject(method = "tickCraftingLogic", at = @At(value = "INVOKE", target = "Lappeng/me/cluster/implementations/CraftingCPUCluster;getCoProcessors()I"), cancellable = true, remap = false)
+    @Inject(method = "tickCraftingLogic", at = @At(value = "INVOKE", target = "Lappeng/me/cluster/implementations/CraftingCPUCluster;getCoProcessors()I"), cancellable = true)
     private void tick(IEnergyService eg, CraftingService cc, CallbackInfo ci){
-        tickcount = this.cluster.getLevel().getGameTime();
+        CTNHCore$tickCount = this.cluster.getLevel().getGameTime();
 
         long stored = 0;
 
@@ -36,7 +40,7 @@ public class CraftingCpuLogicMixin {
         }catch (NoSuchFieldException | IllegalAccessException ignored) {
         }
 
-        if(tickcount % 20 != 0 && stored > 1000000)
+        if(CTNHCore$tickCount % 20 != 0 && stored > 1000000)
         {
             ci.cancel();
         }
