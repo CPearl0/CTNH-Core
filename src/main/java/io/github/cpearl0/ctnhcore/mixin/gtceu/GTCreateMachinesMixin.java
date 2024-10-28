b27fe37dd4d933d4a7ee695f2958fe6b15e362d5
lumine <463305152@qq.com>
2024-10-28 19:13:49 +0800

    fix

--- src/main/java/io/github/cpearl0/ctnhcore/mixin/gtceu/GTCreateMachinesMixin.java
+++ src/main/java/io/github/cpearl0/ctnhcore/mixin/gtceu/GTCreateMachinesMixin.java
@@ -15,7 +15,7 @@ public class GTCreateMachinesMixin {
     @ModifyArg(method = "<clinit>", at = @At(value = "INVOKE", target = "Lcom/gregtechceu/gtceu/common/data/machines/GTCreateMachines;registerTieredMachines(Ljava/lang/String;Ljava/util/function/BiFunction;Ljava/util/function/BiFunction;Ljava/util/function/BiFunction;Lcom/tterrag/registrate/util/nullness/NonNullSupplier;Z[I)[Lcom/gregtechceu/gtceu/common/machine/KineticMachineDefinition;"), index = 1, remap = false)
     private static BiFunction<Integer, ResourceLocation, KineticMachineDefinition> kinetic_input_output_box(BiFunction<Integer, ResourceLocation, KineticMachineDefinition> definitionFactory){
         return (tier, id) -> {
-            return (new KineticMachineDefinition(id, false, (float) GTValues.V[tier] * 4)).setFrontRotation(true);
+            return (new KineticMachineDefinition(id, definitionFactory.apply(1, null).isSource, (float) GTValues.V[tier] * 4)).setFrontRotation(true);
         };
     }
 
